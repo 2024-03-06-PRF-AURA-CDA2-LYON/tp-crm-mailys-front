a8bab0ecbc02136235e181dcd127c5e18d307e8d
maimai <mailys.andrieu.simplon@gmail.com>
2024-10-04 11:09:39 +0200

    accueil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 import { NgModule } from '@angular/core';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 export const routes: Routes = [
+  { path:'',component: AccueilComponent, pathMatch: 'full'},
   { path: 'customers', component: CustomerListComponent },
   { path: 'customers/:id', component: CustomerDetailsComponent },
+
 ];
 
 @NgModule({
