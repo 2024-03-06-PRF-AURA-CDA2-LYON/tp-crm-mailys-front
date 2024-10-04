95e895e6ad6f9b89ff8d51dac01056561d5ab18e
maimai <mailys.andrieu.simplon@gmail.com>
2024-10-04 09:24:40 +0200

    affichage liste customersok

--- src/app/customer-list/customer-list.component.ts
+++ src/app/customer-list/customer-list.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from '../Customer.type';
 import { CustomerService } from '../customer.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.css'
 })
